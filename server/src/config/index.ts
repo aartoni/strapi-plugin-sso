434fd10eb360894459e44432d4ce1f9c5bcc0994
aartoni <alessio@artoni.org>
2026-07-19 17:10:33 +0200

    feat: validate Config types

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -4,26 +4,41 @@ import {
   DISCOVERABLE_FIELDS,
 } from "../utils/config";
 
-export default {
-  default: {
-    discovery: true,
-    rememberMe: true,
-    scopes: "openid profile email groups",
-    familyNameField: "family_name",
-    givenNameField: "given_name",
-  } as Config,
-  validator(config: Config) {
-    const required = config.discovery
-      ? ALWAYS_REQUIRED_FIELDS
-      : [...ALWAYS_REQUIRED_FIELDS, ...DISCOVERABLE_FIELDS];
-    const missing = required.filter((key) => !config[key]);
-    if (missing.length > 0) {
-      throw new Error(`These are required: ${missing.join(", ")}.`);
-    }
+const DEFAULTS = {
+  discovery: true,
+  rememberMe: true,
+  scopes: "openid profile email groups",
+  familyNameField: "family_name",
+  givenNameField: "given_name",
+} as Config;
 
-    const scopes = config.scopes.split(/\s+/);
-    if (!scopes.includes("openid")) {
-      throw new Error("The 'openid' scope is required.");
-    }
-  },
+const TYPE_FIELDS_MAP = {
+  boolean: ["discovery", "rememberMe"] as const,
+  string: ["scopes", "familyNameField", "givenNameField"] as const,
 };
+
+function validator(config: Config) {
+  for (const [type, props] of Object.entries(TYPE_FIELDS_MAP)) {
+    for (const prop of props) {
+      if (typeof config[prop] !== type) {
+        throw new Error(
+          `'${prop}' must be a ${type}, received ${typeof config[prop]}.`,
+        );
+      }
+    }
+  }
+  const required = config.discovery
+    ? ALWAYS_REQUIRED_FIELDS
+    : [...ALWAYS_REQUIRED_FIELDS, ...DISCOVERABLE_FIELDS];
+  const missing = required.filter((key) => !config[key]);
+  if (missing.length > 0) {
+    throw new Error(`These are required: ${missing.join(", ")}.`);
+  }
+
+  const scopes = config.scopes.split(/\s+/);
+  if (!scopes.includes("openid")) {
+    throw new Error("The 'openid' scope is required.");
+  }
+}
+
+export default { default: DEFAULTS, validator };
